9a47083bc367890907a434e52c085b935a481a53
Connor Hengstler <connorwh@student.ubc.ca>
2025-11-16 04:48:07 -0800

    contextualized lessons

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'iconictributes.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.sfu.ca',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
